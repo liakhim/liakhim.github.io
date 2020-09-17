5b5788d6e2db5828a0ae0b7672da7821cf55791b
michael <mmingareev@bk.ru>
2020-09-18 00:53:51 +0300

    test

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue'),
+      meta: {
+        layout: 'default-layout'
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue'),
+      meta: {
+        layout: 'default-layout'
+      }
+    }
+  ]
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="content">
+        <a href="#">
+          <div class="block">
+            <div class="block__title">
+              <p>Standard $35+</p>
+            </div>
+            <div class="block__image">
+              <img src="../assets/standart.jpg" alt="">
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="block">
+            <div class="block__title">
+              <p>Premium $200+</p>
+            </div>
+            <div class="block__image">
+              <img src="../assets/premium.jpg" alt="">
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+<style lang="scss">
+  .home {
+    .container {
+      .content {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        a {
+          .block {
+            margin: 10px;
+            max-width: 430px;
+            border-radius: 10px;
+            overflow: hidden;
+            filter: drop-shadow(4px 4px 24px rgba(0, 0, 0, 0.1));
+          }
+        }
+      }
+    }
+  }
+</style>
